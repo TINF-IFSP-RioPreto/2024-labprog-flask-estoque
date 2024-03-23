b49bc3b4b137ac71eb20d020d90440ad51f1af37
Daniel Correa Lobato <daniel@lobato.org>
2024-03-23 01:09:30 -0300

    Aplicação de "Olá, mundo"

---
+++ app.py
@@ -0,0 +1,7 @@
+from src.factory import create_app
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run(debug=True,
+            host="0.0.0.0",
+            port=5000)
---
+++ src/factory.py
@@ -0,0 +1,34 @@
+import json
+import logging
+import os
+import sys
+
+from flask import Flask, render_template
+
+
+def create_app(config_filename: str = 'config.dev.json') -> Flask:
+    app = Flask(__name__,
+                instance_relative_config=True,
+                static_folder='static',
+                template_folder='templates')
+
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    app.logger.setLevel(logging.DEBUG)
+
+    app.logger.debug("Configurando a aplicação a partir do arquivo '%s'" % (config_filename))
+    try:
+        app.config.from_file(config_filename, load=json.load)
+    except FileNotFoundError:
+        app.logger.fatal("O arquivo de configuração '%s' não existe" % (config_filename))
+        sys.exit(1)
+
+    @app.route('/')
+    @app.route('/index')
+    def index():
+        return render_template('index.jinja', title="Página principal")
+
+    return app
