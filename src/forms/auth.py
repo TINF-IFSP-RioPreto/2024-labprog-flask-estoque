c21f25db9352dd3fe912426607ec90e38515b4fa
Daniel Correa Lobato <daniel@lobato.org>
2024-03-24 01:00:57 -0300

    Features: -- Controller de autenticação: complexidade da senha
    Extensões:
    -- re

--- src/forms/auth.py
+++ src/forms/auth.py
@@ -1,6 +1,50 @@
+import re
+
+from flask import current_app
 from flask_wtf import FlaskForm
 from wtforms.fields.simple import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import InputRequired, Email, EqualTo
+from wtforms.validators import InputRequired, Email, EqualTo, ValidationError
+
+
+class ValidaComplexidadeSenha:
+    def validate_password(self, password):
+        # Definição da complexidade da senha por expressões regulares
+        expressoes = []
+        min = current_app.config.get('PASSWORD_MIN', 8)
+        max = current_app.config.get('PASSWORD_MAX', 64)
+        expressoes.append(f'^(?=.{{{min},{max}}}$)')
+        mensagem = f"A sua senha precisa ter entre {min} e {max} caracteres"
+
+        upper = r'(?=.*[A-Z])'  # Pelo menos 1 letra maiúscula
+        lower = r'(?=.*[a-z])'  # Pelo menos 1 letra minúscula
+        number = r'(?=.*[0-9])'  # Pelo menos um número
+        special = r"(?=.*[!@#\$%\&'\(\)*\+,\-./:;<=>?@\[\\\]^_`{|}~])"  # Pelo menos um caracter especial
+
+        if current_app.config.get('PASSWORD_MAIUSCULA', False):
+            expressoes.append(upper)
+            mensagem = mensagem + ", letras maiúsculas"
+        if current_app.config.get('PASSWORD_MINUSCULA', False):
+            expressoes.append(lower)
+            mensagem = mensagem + ", letras minúsculas"
+        if current_app.config.get('PASSWORD_NUMERO', False):
+            expressoes.append(number)
+            mensagem = mensagem + ", números"
+        if current_app.config.get('PASSWORD_SIMBOLO', False):
+            expressoes.append(special)
+            mensagem = mensagem + ", símbolos especiais"
+
+
+        pattern = "".join(expressoes)
+        print(password.data)
+        print(pattern)
+        # Trocar a última ocorrência de ', ' se houver por ' e ' na mensagem
+        pos = mensagem.rfind(', ')
+        if pos > -1: mensagem = mensagem[:pos] + ' e ' + mensagem[pos + 2:]
+
+        print(mensagem)
+
+        if not re.match(pattern, str(password.data)):
+            raise ValidationError(mensagem)
 
 
 class LoginForm(FlaskForm):
@@ -14,7 +58,7 @@ class LoginForm(FlaskForm):
     submit = SubmitField("Entrar")
 
 
-class SetNewPasswordForm(FlaskForm):
+class SetNewPasswordForm(FlaskForm, ValidaComplexidadeSenha):
     password = PasswordField("Nova senha",
                              validators=[InputRequired(message="É necessário escolher uma senha")])
     password2 = PasswordField("Confirme a nova senha",
