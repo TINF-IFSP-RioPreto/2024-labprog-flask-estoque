f963a3cde3332eda55ec1b239dac28e4b9e70c56
Daniel Corrêa Lobato <daniel@lobato.org>
2024-06-05 20:57:43 -0300

    Recuperação de imagem e de thumbnail

--- src/models/produto.py
+++ src/models/produto.py
@@ -1,8 +1,14 @@
+import io
 import uuid
+from base64 import b64decode
+
+from PIL import Image
+
 from sqlalchemy import Boolean, DECIMAL, ForeignKey, Integer, String, Text, Uuid
 from sqlalchemy.orm import mapped_column, relationship
 from src.modules import db
 from src.models.base_mixin import BasicRepositoryMixin, TimeStampMixin
+
 class Produto(db.Model, BasicRepositoryMixin, TimeStampMixin):
     __tablename__ = 'produtos'
     id = mapped_column(Uuid(as_uuid=True), primary_key=True, default=uuid.uuid4)
@@ -17,3 +23,35 @@ class Produto(db.Model, BasicRepositoryMixin, TimeStampMixin):
 
     categoria = relationship('Categoria',
                              back_populates='lista_de_produtos')
+
+    @property
+    def imagem(self):
+        if not self.possui_foto:
+            saida = io.BytesIO()
+            entrada = Image.new('RGB', (200, 200), (255, 0, 0))
+            formato = "PNG"
+            entrada.save(saida, format=formato)
+            conteudo = saida.getvalue()
+            tipo = 'image/png'
+        else:
+            conteudo = b64decode(self.foto_base64)
+            tipo = self.foto_mime
+        return conteudo, tipo
+
+
+    def thumbnail(self, size: int = 128):
+        if not self.possui_foto:
+            ...
+        else:
+            arquivo = io.BytesIO(b64decode(self.foto_base64))
+            saida = io.BytesIO()
+            entrada = Image.open(arquivo)
+            formato = entrada.format
+            (largura, altura) = entrada.size
+            fator = min(size/largura, size/altura)
+            novo_tamanho = (int(largura * fator), int(altura * fator))
+            entrada.thumbnail(novo_tamanho)
+            entrada.save(saida, format=formato)
+            conteudo = saida.getvalue()
+            tipo = self.foto_mime
+        return conteudo, tipo
--- src/routes/produto.py
+++ src/routes/produto.py
@@ -1,6 +1,6 @@
 from base64 import b64encode
 
-from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask import abort, Blueprint, flash, redirect, render_template, request, Response, url_for
 from flask_login import login_required
 
 from src.forms.produto import ProdutoForm
@@ -47,3 +47,21 @@ def add():
 
     return render_template('produto/add_edit.jinja2', form=form,
                            title="Adicionar novo produto")
+
+
+@bp.route('/imagem/<uuid:id_produto>', methods=['GET'])
+def imagem(id_produto):
+    produto = Produto.get_by_id(id_produto)
+    if produto is None:
+        return abort(404)
+    conteudo, tipo = produto.imagem
+    return Response(conteudo, mimetype=tipo)
+
+@bp.route('/thumbnail/<uuid:id_produto>/<int:size>', methods=['GET'])
+@bp.route('/thumbnail/<uuid:id_produto>', methods=['GET'])
+def thumbnail(id_produto, size=128):
+    produto = Produto.get_by_id(id_produto)
+    if produto is None:
+        return abort(404)
+    conteudo, tipo = produto.thumbnail(size)
+    return Response(conteudo, mimetype=tipo)
